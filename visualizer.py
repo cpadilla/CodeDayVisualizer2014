d29301b3ebb573bbad94c4ba71c77d1b4652cfa6
Christofer Padilla <christoferpadilla@gmail.com>
2014-11-08 16:26:25 -0500

    Added music

--- visualizer.py
+++ visualizer.py
@@ -1,4 +1,9 @@
 from pygame.locals import *
+import pygame
+import time
+import pyaudio
+import wave
+import sys
 
 " class Game(object):"
 
@@ -6,3 +11,30 @@ def run():
     print ("hello!")
 
 run()
+CHUNK = 1024
+
+wf = wave.open('music.wav', 'rb')
+
+# instantiate PyAudio (1)
+p = pyaudio.PyAudio()
+
+# open stream (2)
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+# read data
+data = wf.readframes(CHUNK)
+
+# play stream (3)
+while data != '':
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+# stop stream (4)
+stream.stop_stream()
+stream.close()
+
+# close PyAudio (5)
+p.terminate()
