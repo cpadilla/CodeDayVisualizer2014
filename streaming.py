d8423427007af3c939f9c1441155797c1c72ce39
bojanger <toannguyen1@gmail.com>
2014-11-08 22:15:08 -0500

    streaming.py edit

--- streaming.py
+++ streaming.py
@@ -2,6 +2,17 @@ import numpy as np
 import pyaudio
 import wave
 import sys
+import math
+import matplotlib.pyplot as plt
+import cmath
+
+def rfftfreq(n, d=1.0):
+    if not isinstance(n, int):
+        raise ValueError("n should be an integer")
+    val = 1.0/(n*d)
+    N = n//2 + 1
+    results = np.arange(0, N, dtype=int)
+    return results * val
 
 
 def readMusic():
@@ -28,9 +39,15 @@ def readMusic():
         indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth), data))
 
         # take the fft and square each value
-        fftData = abs(np.fft.rfft(indata))**2
-        nums = fftData[1] / 10000000000
-        print '{0:20f}'.format(fftData[1])
+        fftData = np.abs(np.fft.rfft(indata))
+        fftData = np.delete(fftData,len(fftData)-1)
+        power = np.log10(np.abs(fftData))**2
+        power = np.reshape(power,(8,CHUNK/8))
+        matrix = np.int_(np.average(power,axis=1)/4)
+        # print(freq)
+        print(matrix)
+
+        
 
         # read some more data
         data = wf.readframes(CHUNK)
